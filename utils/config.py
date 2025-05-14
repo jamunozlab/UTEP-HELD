812d3dd4066511db775b0c48954369615f5853be
Diego Armando Juarez <dajuarez@login31.chn.perlmutter.nersc.gov>
2025-05-13 23:07:40 -0700

    solving an error

--- utils/config.py
+++ utils/config.py
@@ -6,5 +6,5 @@ global paths to required directories and executable file name
 home_path = '/global/cfs/projectdirs/m3845/Diego_projects/UTEP-HELD/'
 simulations_path = '/global/cfs/projectdirs/m3845/Diego_projects/NiTi/Phonons/molecular_dynamics_1/Concentration_0.50-0.50/'
 dump_file_root = 'dump.NiTi_200K.'
-pos_file = 'dump.NiTi_200K.0' #'input.txt' #'dump.NiTi_200K.0'
+pos_file = 'atoms_positions.data'#'dump.NiTi_200K.0' #'input.txt' #'dump.NiTi_200K.0'
 fc_filename = 'NiTi_bcc.csv'
