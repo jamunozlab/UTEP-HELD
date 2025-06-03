912a547596c5af797f4a5b23ca5772d15f7ed0ea
Diego Armando Juarez <dajuarez@login09.chn.perlmutter.nersc.gov>
2025-06-03 09:42:45 -0700

    adding some features

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,8 @@ global paths to required directories and executable file name
 """
 
 home_path = '/global/cfs/projectdirs/m3845/Diego_projects/UTEP-HELD/'
-simulations_path = '/global/cfs/projectdirs/m3845/Diego_projects/NiTi/Phonons/molecular_dynamics_1/Concentration_0.50-0.50/'
-dump_file_root = 'dump.NiTi_200K.'
+simulations_path = '/global/cfs/projectdirs/m3845/Diego_projects/NiTi/Phonons/new_test/UTEP-tedehamo/Simulations_antisite/0.50-Ni-0.50-Ti/' #'/global/cfs/projectdirs/m3845/Diego_projects/NiTi/Phonons/molecular_dynamics_1/Concentration_0.50-0.50/'
+dump_file_root = 'dump_NiTi_bcc_B2.' #dump_NiTi_bcc_B2
 pos_file = 'atoms_positions.data'#'dump.NiTi_200K.0' #'input.txt' #'dump.NiTi_200K.0'
 fc_filename = 'NiTi_bcc.csv'
+simulations_path_vacancies = '/global/cfs/projectdirs/m3845/Diego_projects/NiTi/Phonons/new_test/UTEP-tedehamo/Simulations_vacancy/0.50-Ni-0.50-Ti/'
